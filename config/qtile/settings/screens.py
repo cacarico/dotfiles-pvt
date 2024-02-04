b575325472587a6f0976d374052b4bd004ee3799
cacarico <caio.quinilato@gmail.com>
2024-02-04 18:49:59 +0100

    Remove pycache

--- config/qtile/settings/screens.py
+++ config/qtile/settings/screens.py
@@ -2,6 +2,8 @@ from libqtile import bar, widget
 from libqtile.widget import Sep
 from libqtile.config import Screen
 
+wallpaper_path = "/home/cacarico/Pictures/Wallpapers/current"
+
 screens = [
     Screen(
         top=bar.Bar(
@@ -29,7 +31,7 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
-        wallpaper='/home/cacarico/Pictures/Wallpapers/current',
+        wallpaper=wallpaper_path,
         wallpaper_mode='fill',
 
 
@@ -48,6 +50,7 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
+        wallpaper=wallpaper_path,
         wallpaper_mode='fill',
 
 
@@ -66,6 +69,7 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
+        wallpaper=wallpaper_path,
         wallpaper_mode='fill',
 
 

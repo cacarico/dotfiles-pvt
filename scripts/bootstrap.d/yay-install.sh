c155ad77063c740bf80fe1a6d2352482fa6d0446
cacarico <caio.quinilato@gmail.com>
2024-02-04 20:17:19 +0100

    Fix yay install

--- scripts/bootstrap.d/yay-install.sh
+++ scripts/bootstrap.d/yay-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if command -v yay > /dev/null; then
-    pacman -S --needed git base-devel
+if ! command -v yay > /dev/null; then
+    sudo pacman -S --needed git base-devel
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si

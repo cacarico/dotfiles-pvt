8a33e29c31918d5a0de85ba75f9374aa140060d8
cacarico <caio.quinilato@gmail.com>
2024-05-30 23:13:09 +0200

    Move asdf install to python

--- scripts/bootstrap.d/utils/asdf.py
+++ scripts/bootstrap.d/utils/asdf.py
@@ -11,16 +11,58 @@ logger = setup_logger(__name__)
 yaml_file_path = 'scripts/bootstrap.d/packages/asdf.yaml'
 packages = []
 
-# Read the YAML file
-with open(yaml_file_path, 'r') as file:
-    data = yaml.safe_load(file)
-    for package in data['packages']:
+def load_yaml(file_path):
+    """Load YAML data from a file"""
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+def parse_packages(data):
+    """Parse package information from YAML data"""
+    packages = []
+    for package in data['global']:
         package_info = {"package": package['name'], "version": package['versions']}
         packages.append(package_info)
+    return packages
 
-def install_packages():
+def install_package(package, version):
+    """Install a specific version of a package using asdf"""
+    add_plugin = f"asdf plugin add {package}"
+    install_package = f"asdf install {package} {version}"
+    set_package_global = f"asdf global {package} {version}"
+
+    try:
+        logger.info(f"Adding plugin for {package}")
+        subprocess.run(add_plugin, shell=True, check=True)
+
+        logger.info(f"Installing {package} version {version}")
+        subprocess.run(install_package, shell=True, check=True)
+
+        logger.info(f"Setting {package} version {version} globally")
+        subprocess.run(set_package_global, shell=True, check=True)
+
+        logger.info(f"Successfully installed {package} version {version}")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to install {package} version {version}. Error: {e}")
+
+def install_packages(packages):
+    """Install all packages from the list"""
     for package in packages:
-        for version in package['versions']:
-            print(f"{package['version']}")
+        versions = package['version']
 
-    return None
+        # Ensure versions is a list
+        if not isinstance(versions, list):
+            versions = [versions]
+
+        for version in versions:
+            install_package(package['package'], version)
+
+def main():
+    # Load and parse the YAML file
+    data = load_yaml(yaml_file_path)
+    packages = parse_packages(data)
+
+    # Install all packages
+    install_packages(packages)
+
+if __name__ == "__main__":
+    main()
